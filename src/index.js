acb0916a7de3fa0d81493d63e1a89b47ce27163c
miraejang <mirae611@nate.com>
2022-01-07 22:30:04 +0900

    change file name of AuthService

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import AuthService from './service/authService';
+import AuthService from './service/auth_service';
 import firebaseApp from './service/firebase';
 import ImageUploader from './service/image_uploader';
 import ImageFileInput from './component/image_file_input/image_file_input';
