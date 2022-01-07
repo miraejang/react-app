c3b45a3df5045df28c4e0dd60a594c161eee6fbb
miraejang <mirae611@nate.com>
2022-01-07 22:33:00 +0900

    delete hdaer import

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styles from './app.module.css';
-import Header from './component/header/header';
 import Login from './component/login/login';
 import Maker from './component/maker/maker';
 
